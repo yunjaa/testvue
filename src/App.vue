b7316a06a0a424a9d961a90953f1d74a32be25ca
yj <workyj@gmail.com>
2022-05-14 17:51:11 +0900

    bbb

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <nav>
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About 1</router-link>
+      <router-link to="/about">About 12</router-link>
     </nav>
     <router-view/>
   </div>
