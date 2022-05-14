20dd974049a1e796ef3e307a8f11548cd5e37301
yj <workyj@gmail.com>
2022-05-14 17:17:04 +0900

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,40 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <aa msg="Welcome to Your Vue.js App" :use="true" :name="name"/>
+    <label :for="'inp-'+ j" v-for="(item, j) in list" :key="j" >
+      <input type="radio" :value="item.value" v-model="radioCheck" :id="'inp-'+ j" name="radioCheck">
+      {{item.label}}
+    </label>
+
+    <br>
+    {{radioCheck}}
+
+    <div v-if="radioCheck == 10">
+      은경
+    </div>
+    <div v-else-if="radioCheck == 11">
+      성영
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import aa from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    aa
+  },
+  data(){
+    return {
+      name: '안녕',
+      cc: true,
+
+      radioCheck: '',
+      list: [{label:'김은경', value:'10'},{label:'김성영', value:'11'}]
+    }
   }
 }
 </script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h2 v-if="use">{{ name }}</h2>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -32,9 +33,10 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
   props: {
-    msg: String
+    msg: String,
+    name: String,
+    use:Boolean
   }
 }
 </script>
