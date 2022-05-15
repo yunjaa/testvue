493f897e2558f97dff1632edf82c24df93d4d12e
yj <workyj@gmail.com>
2022-05-15 21:39:18 +0900

    test

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+<<<<<<< Updated upstream
     <HelloWorld :msg="msg" :name="name" :use="use"/>
+=======
+    <HelloWorld :msg="msg" :name="name" :use="use" />
+>>>>>>> Stashed changes
   </div>
 </template>
 
@@ -13,11 +17,19 @@ export default {
   name: 'HomeView',
   components: {
     HelloWorld
+<<<<<<< Updated upstream
   }, 
   data: function() {
     return {
       msg: 'Welcome to Your Vue.js App',
       name: 'aa',
+=======
+  },
+  data: function() {
+  return {
+      msg: 'Welcome to Your Vue.js App',
+      name: 'workj',
+>>>>>>> Stashed changes
       use: true
     };
   }
