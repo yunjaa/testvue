fc50d2b981682c07e6dbf4cce4ebf2037af9202c
yj <workyj@gmail.com>
2022-05-15 21:45:19 +0900

    오류 수정

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-<<<<<<< Updated upstream
-    <HelloWorld :msg="msg" :name="name" :use="use"/>
-=======
     <HelloWorld :msg="msg" :name="name" :use="use" />
->>>>>>> Stashed changes
   </div>
 </template>
 
